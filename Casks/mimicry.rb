db9d886ab11a9fc4f7d03c7f298b234b0b4ee19f
kusumotoa <mshrksmt@gmail.com>
2025-12-20 22:10:14 +0900

    Add mimicry cask

---
+++ Casks/mimicry.rb
@@ -0,0 +1,24 @@
+cask "mimicry" do
+  version "1.0.0"
+  sha256 "68dcfb32a72e1a92e779b75e34314f3b5f47b1e5cb59478c8abab8d19c20057d"
+
+  url "https://github.com/kusumotoa/mimicry-releases/releases/download/v#{version}/Mimicry-#{version}-arm64.dmg"
+  name "Mimicry"
+  desc "HTTP/HTTPS proxy & mock tool for iOS/Android development"
+  homepage "https://github.com/kusumotoa/Mimicry"
+
+  livecheck do
+    url "https://github.com/kusumotoa/mimicry-releases/releases/latest"
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Mimicry.app"
+
+  zap trash: [
+    "~/Library/Application Support/mimicry",
+    "~/Library/Preferences/com.mimicry.app.plist",
+    "~/Library/Caches/com.mimicry.app",
+  ]
+end
