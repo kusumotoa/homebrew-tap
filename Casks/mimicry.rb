63a9382ea49dc71b0217034a3550a06c5a3c1952
kusumotoa <mshrksmt@gmail.com>
2026-02-26 13:20:21 +0900

    fix: DMG ファイル名を Tauri v2 形式に更新

--- Casks/mimicry.rb
+++ Casks/mimicry.rb
@@ -2,7 +2,7 @@ cask "mimicry" do
   version "2.1.9"
   sha256 "43dac47c42f79fe948fe1d704b6d5ecd3a1dd4a3ac45f799abcd1f125b23492f"
 
-  url "https://github.com/kusumotoa/mimicry-releases/releases/download/v#{version}/Mimicry-#{version}-arm64.dmg"
+  url "https://github.com/kusumotoa/mimicry-releases/releases/download/v#{version}/Mimicry_#{version}_aarch64.dmg"
   name "Mimicry"
   desc "HTTP/HTTPS proxy & mock tool for iOS/Android development"
   homepage "https://github.com/kusumotoa/Mimicry"
