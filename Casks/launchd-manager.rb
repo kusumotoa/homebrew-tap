bd224d0a25542356797624b62b8975f1ee6d6a67
Kusumoto <mshrksmt@gmail.com>
2026-02-27 00:08:21 +0900

    Fix URL format for launchd-manager download

--- Casks/launchd-manager.rb
+++ Casks/launchd-manager.rb
@@ -2,7 +2,7 @@ cask "launchd-manager" do
   version "0.4.0"
   sha256 "820eaeab657fe5695f6e426987b1695a61b80f054d9874febd72692da9508514"
 
-  url "https://github.com/kusumotoa/launchd-manager-releases/releases/download/v#{version}/launchd%20Manager_#{version}_aarch64.dmg"
+  url "https://github.com/kusumotoa/launchd-manager-releases/releases/download/v#{version}/launchd.Manager_#{version}_aarch64.dmg"
   name "launchd Manager"
   desc "macOS launchd Agent Manager"
   homepage "https://github.com/kusumotoa/launchd-manager"
