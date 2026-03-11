5ff0eb02d9174552783eb9eb12dd82bfd3016706
kusumotoa <mshrksmt@gmail.com>
2026-03-12 02:58:12 +0900

    chore: update FluffDrop to v1.0.1

--- Casks/fluffdrop.rb
+++ Casks/fluffdrop.rb
@@ -1,5 +1,5 @@
 cask "fluffdrop" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/kusumotoa/fluffdrop-releases/releases/download/v#{version}/FluffDrop_#{version}_aarch64.dmg"
