4a34c5c518a0046a4b0d7085a88b4012c5fbec94
kusumotoa <mshrksmt@gmail.com>
2026-03-15 00:14:47 +0900

    chore: add Breezy cask

---
+++ Casks/breezy.rb
@@ -0,0 +1,24 @@
+cask "breezy" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/kusumotoa/breezy-releases/releases/download/v#{version}/Breezy_#{version}_aarch64.dmg"
+  name "Breezy"
+  desc "macOS menu bar app to sync directories between Macs via iCloud"
+  homepage "https://github.com/kusumotoa/Breezy"
+
+  livecheck do
+    url "https://github.com/kusumotoa/breezy-releases/releases/latest"
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sequoia"
+
+  app "Breezy.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kusumoto.breezy",
+    "~/Library/Preferences/com.kusumoto.breezy.plist",
+    "~/Library/Caches/com.kusumoto.breezy",
+  ]
+end
