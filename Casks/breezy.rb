3e57c19a26cc2a38bdb298af6b93d94ca838e237
kusumotoa <mshrksmt@gmail.com>
2026-04-06 18:59:28 +0900

    chore: update Breezy to v0.2.0

--- Casks/breezy.rb
+++ Casks/breezy.rb
@@ -1,5 +1,5 @@
 cask "breezy" do
-  version "0.1.1"
+  version "0.2.0"
   sha256 :no_check
 
   url "https://github.com/kusumotoa/breezy-releases/releases/download/v#{version}/Breezy_#{version}_aarch64.dmg"
