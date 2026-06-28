96b93eddcfa1e5ae74203a3a1d6b4f9c0fa0036d
kusumotoa <mshrksmt@gmail.com>
2026-06-28 13:43:46 +0900

    chore: update Mimicry to v6.4.13 (+ versioned cask)

--- Casks/mimicry.rb
+++ Casks/mimicry.rb
@@ -1,6 +1,6 @@
 cask "mimicry" do
-  version "6.4.12"
-  sha256 "178e7df1ff13f5c33325b8289a3b1cf8be902cb25202a08d382181091e5af0b8"
+  version "6.4.13"
+  sha256 "6aa4e6d85b54407563a7eb3cd91a434acbdafe494fec7d7ab7d1c55cc7990cba"
 
   url "https://github.com/kusumotoa/mimicry-releases/releases/download/v#{version}/Mimicry_#{version}_universal.dmg"
   name "Mimicry"
