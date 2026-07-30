f72bf743c7db56126956bda640e64e75bfc7fa47
kusumotoa <mshrksmt@gmail.com>
2026-07-31 01:57:22 +0900

    chore: update appforceps to 2.0.0

--- Casks/appforceps.rb
+++ Casks/appforceps.rb
@@ -1,6 +1,6 @@
 cask "appforceps" do
-  version "1.0.6"
-  sha256 "0840f02fa9a45e3ec679f95ee506ae43b69df8ca7f6b9e802675315829391cff"
+  version "2.0.0"
+  sha256 "35b8b17e29046a071557e6854d5b891a81f467d9c69fc601b0ec660ee1988015"
 
   url "https://github.com/kusumotoa/AppForceps-releases/releases/download/v#{version}/AppForceps_#{version}_aarch64.dmg"
   name "AppForceps"
